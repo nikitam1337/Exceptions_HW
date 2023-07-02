9c5b6bbf108c5a21e3d5b53af232531000025e2d
nikitam1337 <nikitam.l@mail.ru>
2023-07-02 16:14:54 +0300

    HW_2 Task4 compleated

---
+++ HW_2/Task4.java
@@ -0,0 +1,29 @@
+package HomeWork.HW_2;
+
+import java.util.Scanner;
+
+/*
+ * Разработайте программу, которая выбросит Exception, 
+ * когда пользователь вводит пустую строку. 
+ * Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+        try {
+            nameMethod();
+        } catch (Exception e) {
+            System.err.println("Пустыне строки вводить нельзя");
+        }
+    }
+
+    public static void nameMethod() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите любые значения и нажмите ENTER");
+        String input = scanner.nextLine();
+        if (input.length() == 0) throw new RuntimeException();
+        else{
+            System.out.println(input);
+        }
+        scanner.close();
+    }
+}
